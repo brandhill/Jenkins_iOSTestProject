9008afe5648f5563f49f354e449eba8be5102db5
hill.hung <brandhill.g@gmail.com>
2016-10-19 16:46:15 +0800

    init project

---
+++ JenkinsTestProject/JenkinsTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTestProject
+//
+//  Created by Hill on 19/10/2016.
+//  Copyright © 2016 mitsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsTestProject/JenkinsTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTestProject
+//
+//  Created by Hill on 19/10/2016.
+//  Copyright © 2016 mitsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
